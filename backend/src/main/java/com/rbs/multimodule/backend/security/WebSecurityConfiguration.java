82243fc429f592e0576946b63f7b21505855939a
Ahmed Al-Salih <ahmed.al-salih@delhaize.com>
2020-05-13 16:36:00 -0400

    fix the base-url

--- backend/src/main/java/com/rbs/multimodule/backend/security/WebSecurityConfiguration.java
+++ backend/src/main/java/com/rbs/multimodule/backend/security/WebSecurityConfiguration.java
@@ -61,7 +61,7 @@ public class WebSecurityConfiguration  extends WebSecurityConfigurerAdapter  {
 	  return new WebMvcConfigurerAdapter() {
 	    @Override
 	    public void addCorsMappings(CorsRegistry registry) {
-	      registry.addMapping("/**").allowedOrigins("http://localhost:8080").allowedMethods("GET", "POST", "PUT", "PATCH");;
+	      registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST", "PUT", "PATCH");;
 	    }
 	  };
 	}
--- frontend/src/config/axios.js
+++ frontend/src/config/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  baseURL: 'http://localhost:8098/api',
+  baseURL: '/api',
   auth: {
     username: 'admin',
     password: '123'
